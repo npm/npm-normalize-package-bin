585662bca1bb361c72b6822bf6a2389e53d545f6
isaacs <i@izs.me>
2019-12-09 12:30:56 -0800

    tests

---
+++ test/nobin.js
@@ -0,0 +1,30 @@
+const normalize = require('../')
+const t = require('tap')
+
+// all of these just delete the bins, so expect the same value
+const expect = { name: 'hello', version: 'world' }
+
+t.test('no bin in object', async t => {
+  const pkg = { name: 'hello', version: 'world' }
+  t.strictSame(normalize(pkg), expect)
+})
+
+t.test('empty string bin in object', async t => {
+  const pkg = { name: 'hello', version: 'world', bin: '' }
+  t.strictSame(normalize(pkg), expect)
+})
+
+t.test('false bin in object', async t => {
+  const pkg = { name: 'hello', version: 'world', bin: false }
+  t.strictSame(normalize(pkg), expect)
+})
+
+t.test('null bin in object', async t => {
+  const pkg = { name: 'hello', version: 'world', bin: null }
+  t.strictSame(normalize(pkg), expect)
+})
+
+t.test('number bin', async t => {
+  const pkg = { name: 'hello', version: 'world', bin: 42069 }
+  t.strictSame(normalize(pkg), expect)
+})
---
+++ test/string.js
@@ -0,0 +1,20 @@
+const normalize = require('../')
+const t = require('tap')
+
+t.test('benign string', async t => {
+  const pkg = { name: 'hello', version: 'world', bin: 'hello.js' }
+  const expect = { name: 'hello', version: 'world', bin: { hello: 'hello.js' } }
+  t.strictSame(normalize(pkg), expect)
+})
+
+t.test('slashy string', async t => {
+  const pkg = { name: 'hello', version: 'world', bin: '/etc/passwd' }
+  const expect = { name: 'hello', version: 'world', bin: { hello: 'etc/passwd' } }
+  t.strictSame(normalize(pkg), expect)
+})
+
+t.test('dotty string', async t => {
+  const pkg = { name: 'hello', version: 'world', bin: '../../../../etc/passwd' }
+  const expect = { name: 'hello', version: 'world', bin: { hello: 'etc/passwd' } }
+  t.strictSame(normalize(pkg), expect)
+})
