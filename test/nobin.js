af3638b2b85ec170747887399a61d4dfc6d5379e
Michael Smith <owlstronaut@github.com>
2025-12-11 10:37:29 -0800

    chore: swap from tap to built-in node:test

--- test/nobin.js
+++ test/nobin.js
@@ -1,35 +1,36 @@
 const normalize = require('../')
-const t = require('tap')
+const { test } = require('node:test')
+const assert = require('node:assert')
 
 // all of these just delete the bins, so expect the same value
 const expect = { name: 'hello', version: 'world' }
 
-t.test('no bin in object', async t => {
+test('no bin in object', async () => {
   const pkg = { name: 'hello', version: 'world' }
-  t.strictSame(normalize(pkg), expect)
-  t.strictSame(normalize(normalize(pkg)), expect, 'double sanitize ok')
+  assert.deepStrictEqual(normalize(pkg), expect)
+  assert.deepStrictEqual(normalize(normalize(pkg)), expect, 'double sanitize ok')
 })
 
-t.test('empty string bin in object', async t => {
+test('empty string bin in object', async () => {
   const pkg = { name: 'hello', version: 'world', bin: '' }
-  t.strictSame(normalize(pkg), expect)
-  t.strictSame(normalize(normalize(pkg)), expect, 'double sanitize ok')
+  assert.deepStrictEqual(normalize(pkg), expect)
+  assert.deepStrictEqual(normalize(normalize(pkg)), expect, 'double sanitize ok')
 })
 
-t.test('false bin in object', async t => {
+test('false bin in object', async () => {
   const pkg = { name: 'hello', version: 'world', bin: false }
-  t.strictSame(normalize(pkg), expect)
-  t.strictSame(normalize(normalize(pkg)), expect, 'double sanitize ok')
+  assert.deepStrictEqual(normalize(pkg), expect)
+  assert.deepStrictEqual(normalize(normalize(pkg)), expect, 'double sanitize ok')
 })
 
-t.test('null bin in object', async t => {
+test('null bin in object', async () => {
   const pkg = { name: 'hello', version: 'world', bin: null }
-  t.strictSame(normalize(pkg), expect)
-  t.strictSame(normalize(normalize(pkg)), expect, 'double sanitize ok')
+  assert.deepStrictEqual(normalize(pkg), expect)
+  assert.deepStrictEqual(normalize(normalize(pkg)), expect, 'double sanitize ok')
 })
 
-t.test('number bin', async t => {
+test('number bin', async () => {
   const pkg = { name: 'hello', version: 'world', bin: 42069 }
-  t.strictSame(normalize(pkg), expect)
-  t.strictSame(normalize(normalize(pkg)), expect, 'double sanitize ok')
+  assert.deepStrictEqual(normalize(pkg), expect)
+  assert.deepStrictEqual(normalize(normalize(pkg)), expect, 'double sanitize ok')
 })
